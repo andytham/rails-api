3bbe299853e7435afd623e28756a3af49b27720c
andy tham <thamandyy@gmail.com>
2018-04-03 13:00:46 -0400

    test cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://vengeful-eggs.surge.sh/'
 
     resource '*',
       headers: :any,
