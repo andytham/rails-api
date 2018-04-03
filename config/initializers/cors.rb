6ecfab6cbe575cd25ca04bf2f7bc0f9652e48425
andy tham <thamandyy@gmail.com>
2018-04-03 14:18:54 -0400

    cors hello world

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'andytham.com'
+    origins 'hello-world.andytham.com'
 
     resource '*',
       headers: :any,
