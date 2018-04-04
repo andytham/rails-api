0bfaa3cf150b3ee8aa25e9ea72db7ae28b1f6a0f
andy tham <thamandyy@gmail.com>
2018-04-04 14:50:54 -0400

    cors everywhere

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'hello-world.andytham.com'
+    # origins 'hello-world.andytham.com'
+    origins '*' #allow any site to use at the moment
 
     resource '*',
       headers: :any,
